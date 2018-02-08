17e49b97f48d699f3710a9c2e829fa4b2408b286
rdelgado <ezrichie2006@live.com>
2018-02-07 17:08:45 -0800

    Added MVC pages and more for dutch treat learning app.

--- DutchTreat2/DutchTreat2/Controllers/AppController.cs
+++ DutchTreat2/DutchTreat2/Controllers/AppController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DutchTreat2.ViewModels;
 
 namespace DutchTreat2.Controllers
 {
@@ -12,5 +13,27 @@ namespace DutchTreat2.Controllers
         {
             return View();
         }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            //throw new InvalidOperationException("Bad things happened.");
+
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+
     }
 }
