8ac556e3e84d2cd0f42751da2d8613b589a7ac17
rdelgado <ezrichie2006@live.com>
2018-02-07 18:12:41 -0800

    added services

--- DutchTreat2/DutchTreat2/Controllers/AppController.cs
+++ DutchTreat2/DutchTreat2/Controllers/AppController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DutchTreat2.ViewModels;
+using DutchTreat2.Services;
 
 namespace DutchTreat2.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -27,11 +34,9 @@ namespace DutchTreat2.Controllers
         {
             if (ModelState.IsValid)
             {
-
-            }
-            else
-            {
-
+                _mailService.SendMessage("test@test.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message} ");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
             }
 
             return View();
--- DutchTreat2/DutchTreat2/Startup.cs
+++ DutchTreat2/DutchTreat2/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using DutchTreat2.Services;
 
 namespace DutchTreat2
 {
@@ -15,6 +16,7 @@ namespace DutchTreat2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
             services.AddMvc();
         }
 
