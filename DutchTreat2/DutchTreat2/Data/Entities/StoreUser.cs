247968c4522d8e6927fea049db0ff5cbcbfea8f0
rdelgado <ezrichie2006@live.com>
2018-02-24 20:46:54 -0800

    Updated to working version due to changes in pluralsight lecturer files and actual code

--- DutchTreat2/DutchTreat2/Data/Entities/StoreUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace DutchTreat2.Data.Entities{
-    public class StoreUser : IdentityUser{
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        
-    }
-}
