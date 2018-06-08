ad46f3f4a412089010b42954744b4e090469cf31
Richard Delgado <richarddelgado@Richards-Mac-Pro.local>
2018-06-07 22:00:11 -0700

    fixed styling of welcome component and product components except for star

--- LearningAngApp/src/app/app.component.ts
+++ LearningAngApp/src/app/app.component.ts
@@ -6,16 +6,15 @@ import { LegendService } from './services/legend.service';
     selector: 'pm-root',
     template: `
     <div>
-      <nav class='navbar navbar-default'>
-        <div class='container-fluid'>
-          <a class='navbar-brand'>{{pageTitle}}</a>
-          <ul class='nav navbar-nav'>
-            <li><a [routerLink]="['/welcome']">Home</a></li>
-            <li><a [routerLink]="['/products']">Product List</a></li>
-            <li><a [routerLink]="['/timecards']">Timecard List</a></li>
-          </ul>
-        </div>
-      </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+      <li class="nav-item"><a [routerLink]="['/welcome']" class="nav-link">Home</a></li>
+      <li class="nav-item"><a [routerLink]="['/products']" class="nav-link">Product List</a></li>
+      <li class="nav-item"><a [routerLink]="['/timecards']" class="nav-link">Timecard List</a></li>
+      </ul>
+    </div>
+  </nav>
       <div>
         <router-outlet></router-outlet>
       </div>
@@ -26,6 +25,10 @@ import { LegendService } from './services/legend.service';
 
 // <h1>{{pageTitle}}</h1>
 // <pm-legends></pm-legends>
+// <form class="form-inline my-2 my-lg-0">
+//         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+//         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//       </form>
 
 export class AppComponent {
   pageTitle: string = 'Acme Product Development';
