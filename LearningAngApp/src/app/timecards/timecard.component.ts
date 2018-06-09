0686e1cc4a9a8fa63890da68688563ce231b9079
Richard Delgado <richarddelgado@Richards-Pro.T-mobile.com>
2018-06-09 01:23:24 -0700

    added sample form for radio inputs

--- LearningAngApp/src/app/timecards/timecard.component.ts
+++ LearningAngApp/src/app/timecards/timecard.component.ts
@@ -16,9 +16,31 @@ export class TimecardComponent implements OnInit {
   private _timecardDays: ITimecardDay[];
   public timecardDays: ITimecardDay[];
 
+  public timecardWeek: Date;
+  public timecardDate: Date;
+  public radioItem: any;
+  public radioItem1: boolean;
+  public radioItem2: boolean;
+  public radioItem3: boolean;
+  public radioItem4: boolean;
+  public selectedRadio: number;
+
+  public isSelected1: boolean;
+  public isSelected2: boolean;
+  public isSelected3: boolean;
+  public isSelected4: boolean;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.timecardDate = new Date();
+
+    this.radioItem1 = false;
+    this.radioItem2 = false;
+    this.radioItem3 = false;
+    this.radioItem4 = false;
+
     // const now: Date = new Date();
     // const timecardInDay: NgbDateStruct = {
     //   year: 1,
@@ -45,6 +67,43 @@ export class TimecardComponent implements OnInit {
     //   sampleTimecardDay
     // ];
     // this._timecardDays = this.timecardDays;
+  }
+
+  onRadioChange(selectedValue) {
+    // this.selectedRadio = selectedValue;
+
+    this.isSelected1 = false;
+    this.isSelected2 = false;
+    this.isSelected3 = false;
+    this.isSelected4 = false;
+
+    switch (selectedValue) {
+      case 1:
+      {
+        this.isSelected1 = true;
+        break;
+      }
+        case 2:
+        {
+          this.isSelected2 = true;
+          break;
+        }
+        case 3:
+        {
+          this.isSelected3 = true;
+          break;
+        }
+        case 4:
+        {
+          this.isSelected4 = true;
+          break;
+        }
+        default:
+        {
+          // this.selectedRadio = false;
+        }
+    }
+
   }
 
 }
