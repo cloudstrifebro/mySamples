0c6c76e7f6c5911f82c0b73f6475dc5395a6f6ca
cloudstrifebro <ezrichie2006@gmail.com>
2018-07-25 19:43:57 -0700

    Made changes to ngbootstrap alert

--- LearningAngApp/src/app/home/welcome.component.ts
+++ LearningAngApp/src/app/home/welcome.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Input, Component } from '@angular/core';
 
 @Component({
     templateUrl: './welcome.component.html'
 })
+
 export class WelcomeComponent {
     public pageTitle: string = 'Welcome';
+    @Input()
+    public alerts: Array<IAlert> = [];
+    private backup: Array<IAlert>;
+
+    constructor() {
+        let successAl: IAlert = {
+            id: 1,
+            type: 'success',
+            message: 'This is a success alert.'
+        };
+
+        this.alerts.push(successAl);
+
+        //   this.backup = this.alerts.map((alert: IAlert) => Object.assign({}, alert));
+        }
+
+        public closeAlert(alert: IAlert) {
+            const index: number = this.alerts.indexOf(alert);
+            this.alerts.splice(index, 1);
+          }
 }
+
+export interface IAlert {
+    id: number;
+    type: string;
+    message: string;
+  }
